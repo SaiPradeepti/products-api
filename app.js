5d15b5b22eeaf0f8229c95b3414fb64851ff42e8
CodeSandbox <support@codesandbox.io>
2023-11-28 03:47:14 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const products = JSON.parse(fs.readFileSync("./data.json"));
+
+app.get("/api/v1/products", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      products: products,
+    },
+  });
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id > products.length) {
+    res.status(400).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  const product = products.filter((item) => item.id === id);
+  res.status(200).json({
+    status: "success",
+    data: {
+      product,
+    },
+  });
+});
+
+const port = 3000;
+app.listen(3000, () => {
+  console.log(`App is running on port ${port}`);
+});
