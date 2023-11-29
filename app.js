c5362711c257f5555e550a55e50aca260f3fb477
Sai Pradeepti <saipradeepti14@gmail.com>
2023-11-29 15:56:14 +0000

    Creating and mounting router

--- app.js
+++ app.js
@@ -97,13 +97,21 @@ const deleteProduct = (req, res) => {
 // app.post("/api/v1/products", createProduct);
 // app.patch("/api/v1/products/:id", updateProduct);
 // app.delete("/api/v1/products/:id", deleteProduct);
+
 // refactoring the above routes
-app.route("/api/v1/products").get(getAllProducts).post(createProduct);
-app
-  .route("/api/v1/products/:id")
-  .get(getProduct)
-  .patch(updateProduct)
-  .delete(deleteProduct);
+// app.route("/api/v1/products").get(getAllProducts).post(createProduct);
+// app
+//   .route("/api/v1/products/:id")
+//   .get(getProduct)
+//   .patch(updateProduct)
+//   .delete(deleteProduct);
+
+// creating router and mounting on the above routes
+app.use("/api/v1/products", router); // subapplication for the routes
+const router = express.Router(); // this router is a middleware
+
+router.route("/").get(getAllProducts).post(createProduct);
+router.route("/:id").get(getProduct).patch(updateProduct).delete(deleteProduct);
 
 const port = 3000;
 app.listen(port, () => {
